31a81f9a85e6db21124bdfc3e176d94c1074d907
Achmad Amri <achmad.amri@gmail.com>
2022-02-22 16:29:35 +0700

    update

--- bsd_api_dms_member.sql
+++ bsd_api_dms_member.sql
@@ -269,4 +269,4 @@ SET character_set_client = @saved_cs_client;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-02-16 16:15:22
+-- Dump completed on 2022-02-22 16:28:54
